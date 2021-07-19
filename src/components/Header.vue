ddd704eb2d2e1b164ea5ec3a7c42e4e90f42e2de
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-07-19 19:33:17 +0000

    create Button component and add it into Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,18 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
+    <Button />
   </header>
 </template>
 
 <script>
+import Button from './Button.vue';
+
 export default {
   name: "Header",
+  components : {
+    Button
+  },
   // props: ['title']    OR:
   props: {
     title: String, //you can verify specific type

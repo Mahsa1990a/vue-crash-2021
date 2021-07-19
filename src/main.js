25b00a189559ea03daf125032ad1691bac8cd6e6
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-07-19 19:05:12 +0000

    setup

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// entry point of view
+
+import { createApp } from 'vue'; // importing createApp methode from Vue
+import App from './App.vue'; // importing our root component
+
+createApp(App).mount('#app');

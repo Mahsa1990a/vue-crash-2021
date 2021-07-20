ca684f23a21f829ebda55a0e87dc471fef17c626
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-07-20 13:50:42 +0000

    added unique key, and outer div is for getting rid of err

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- here we want to taking an arr and loop through them and output single task component for each one-->
-  <!-- v-for we can loop through something -->
+  <!-- v-for we can loop through something // outer div is for getting rid of err-->
   <div>
     <div v-for='task in tasks' :key="task.id">
       <h3>{{ task.text }}</h3>

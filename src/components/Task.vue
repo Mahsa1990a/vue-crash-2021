266bccce7fc894ce493f0cab01e1d6ef70f9b2c7
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-07-20 14:20:28 +0000

    added some styles

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -14,5 +14,19 @@ export default {
 }
 </script>
 
-<style>
+<style scope>
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder { /* if reminder is T will have border on left side */
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
